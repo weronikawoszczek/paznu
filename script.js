e5b94c6110fe49dbee582e995e0a53c70a852312
weronikawoszczek <37816261+weronikawoszczek@users.noreply.github.com>
2020-01-22 18:18:49 +0100

    add websites

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 $( ".pin" ).click(function() {
     $( "#info" ).slideToggle();
 });
+
+$( "#nawiguj" ).click(function() {
+    console.log("aaa");
+    var imageUrl = "img/map-trasa.png";
+    $( ".map" ).css("background-image", 'url(' + imageUrl + ')');
+
+});
